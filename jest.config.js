ea1c07014223a11548cb601a4f9c15183152b387
asifjamal-persistent <126173120+asifjamal-persistent@users.noreply.github.com>
2023-03-28 13:30:15 +0530

    Jest Config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,19 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
-    ...jestConfig,
+    moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+    transform: {
+      '^.+\\.vue$': 'vue-jest',
+      '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+        'jest-transform-stub',
+      '^.+\\.(js|jsx)?$': 'babel-jest'
+    },
     moduleNameMapper: {
-
-        '^lightning/platformShowToastEvent$':
-    
-           
-            '/Users/asif_jamal/Downloads/ShowToastEvent.js'
-    
-       },
-    
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
-};
+      '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    snapshotSerializers: ['jest-serializer-vue'],
+    testMatch: [
+      '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'
+    ],
+    transformIgnorePatterns: ['<rootDir>/node_modules/']
+  };
